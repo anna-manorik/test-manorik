4a19a0e34833a59b12d469ac4d09aec0d2319faa
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2021-11-10 13:17:59 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const { email, password } = event.target.elements;
+
+    const allInputs = {
+        email: email.value,
+        password: password.value,
+    }
+
+    if(allInputs.email === "" || allInputs.password === ""){
+        alert("Inputs can't be empty!");
+    }
+
+    console.log(allInputs);  
+
+    const inputs = document.querySelectorAll("input");
+
+    inputs.forEach(input => {
+        input.value = "";
+    })
+})

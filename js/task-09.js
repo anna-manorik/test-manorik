5d560758e96ea6d2ecd950b602b20bc53b10cfa7
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2021-11-10 13:56:52 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorBtn = document.querySelector(".change-color");
+const colorName = document.querySelector(".color");
+const body = document.querySelector("body");
+
+colorBtn.addEventListener("click", () => {
+  const currentColor = getRandomHexColor();
+  colorName.textContent = currentColor;
+  body.style.backgroundColor = currentColor;
+})

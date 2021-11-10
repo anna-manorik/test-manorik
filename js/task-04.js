033ff8917499e66f6e5bc1167f9e583f143718e9
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2021-11-10 10:24:02 +0200

    Create task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,20 @@
+let counterValue = 0;
+
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+const valueSpan = document.getElementById("value");
+
+
+decrementBtn.addEventListener(`click`, () => {
+    counterValue -= 1;
+    valueSpan.textContent = counterValue;
+});
+
+incrementBtn.addEventListener(`click`, () => {
+    counterValue += 1;
+    valueSpan.textContent = counterValue;
+});
+
+
+
+

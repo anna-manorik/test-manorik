4e6f5bb80cc6782f4406d74a7d82c4faf688f2fd
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2021-11-10 13:56:54 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -5,13 +5,11 @@ textInput.addEventListener("input", (event) => {
     
   output.textContent = event.currentTarget.value;
 
+  if(event.currentTarget.value === ""){
+    output.textContent = "Anonymous";
+  };
+
 });
 
-// console.log(input);
 
-// if(textInput.textContent === ""){
-
-//     output.textContent = "Anonymous";
-
-// }
 

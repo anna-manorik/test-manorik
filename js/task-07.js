8f28d3ccf620356d18d00295baeb711ecb2d9a87
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2021-11-14 00:41:51 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,7 @@
 const range = document.getElementById("font-size-control");
 const text = document.getElementById("text");
 
-range.addEventListener("change", (event) => {
+range.addEventListener("input", (event) => {
     text.setAttribute("style", `font-size: ${event.currentTarget.value}px`);
 })
 
